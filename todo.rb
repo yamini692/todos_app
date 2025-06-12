265d9d99b78e15fcd4a491589dc867957c804718
yamini692 <yaminithya692@gmail.com>
2025-06-12 15:02:03 +0530

    Adding add task feature

--- todo.rb
+++ todo.rb
@@ -5,3 +5,19 @@ puts "1.Add"
 puts "2.List"
 puts "3.exit"
 
+num =0 
+task =[]
+while num!=3
+  print("Enter Choice : ")
+  num = gets.chomp.to_i
+  case num
+  when 1
+    puts 
+    print "Enter task : "
+    s = gets.chomp
+    task.push(s)
+    puts "task added Successfully"
+  else
+    puts "enter valid Number 1,2 or 3"
+  end 
+end
